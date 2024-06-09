e2974335cffb36d8c8476ba1aa0e5554616494b0
veraafridita30 <veraafriditaa0130@gmail.com>
2024-06-09 14:51:49 +0700

    v

--- backend/app/Http/Controllers/Pengunjung.php
+++ backend/app/Http/Controllers/Pengunjung.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MPengunjung;
 use Illuminate\Http\Request;
 
 class Pengunjung extends Controller
 {
-    //
+    // buat inisialisasi untuk pengambilan model
+    protected $model;
+
+    // buat konstruktor
+    function __construct()
+    {
+        // isi nilai variabel "$model"
+        $this->model = new MPengunjung();
+    }
+
+    // buat fungsi untuk tambah data
+    function viewData()
+    {
+        // jika data pengunjung tidak ada
+        if (count($this->model->viewData()) == 0) {
+            $data = [];
+            $error = 1;
+            $message = "Data Pengunjung Tidak Ditemukan !";
+        }
+        // jika data pengunjung ada/ tersedia
+        else {
+            // ambil medthod "viewData" dari model "MPengunjung"
+            $data = $this->model->viewData();
+            $error = 0;
+            $message = "";
+        }
+
+        return response(["pengunjung" => $data, "error" => $error, "message" => $message], http_response_code());
+    }
 }
