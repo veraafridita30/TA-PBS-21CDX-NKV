bc8fd5894c1a4c3c6df58d4c665e8fdb7c9400ef
veraafridita30 <veraafriditaa0130@gmail.com>
2024-06-09 23:54:31 +0700

    v

--- backend/app/Http/Controllers/Pengunjung.php
+++ backend/app/Http/Controllers/Pengunjung.php
@@ -36,4 +36,90 @@ class Pengunjung extends Controller
 
         return response(["pengunjung" => $data, "error" => $error, "message" => $message], http_response_code());
     }
+
+    // buat fungsi untuk tambah data
+    function saveData(Request $req)
+    {
+        // ambil data email
+        $email = $req->email;
+
+        // jika email sudah ada
+        if (count($this->model->checkSaveData($email)) != 0) {
+            $error = 1;
+            $message = "Data Gagal Disimpan (NPM Sudah Terpakai !)";
+        }
+        // jika email belum ada
+        else {
+            // ambil request
+            $nama = $req->nama;
+            $telepon = $req->telepon;
+            $alamat = $req->alamat;
+
+            // proses simpan data
+            $this->model->saveData($email, $nama, $telepon, $alamat);
+
+            $error = 0;
+            $message = "Data Berhasil Disimpan";
+        }
+
+        return response(["error" => $error, "message" => $message], http_reponse_code());
+    }
+
+    // buat fungsi hapus data
+    function deleteData($email)
+    {
+        // cek apakah data pengunjung (email) tersedia/tidak pada model checkData
+        // jika data tersedia
+        if (count($this->model->checkData($email)) == 1) {
+            // panggil model "deleteData"
+            $this->model->deleteData($email);
+
+            $error = 0;
+            $message = "Data Berhasil Dihapus";
+        }
+        // jika data tidak tersedia
+        else {
+            $error = 1;
+            $message = "Data Gagal Dihapus !";
+        }
+        return response(["error" => $error, "message" => $message], http_response_code());
+    }
+
+    // buat fungsi untuk detail data
+    function detailData($email)
+    {
+        // isi nilai variabel "result" dari fungsi "detailData" dari model "MPengunjung" sesuai dengan isi parameter "id"
+        $data = $this->model->detailData($email);
+
+        // kembalikan nilai variabel "result" ke dalam object "pengunjung"
+        return response(["mahasiswa" => $data], http_response_code());
+    }
+
+    // buat fungsi untuk edit data
+    function editData($email_lama, Request $req)
+    {
+        // ambil data email
+        $email = $req->email;
+
+        // cek apakah data pengunjung (email) tersedia/tidak pada model checkEditData
+        // jika ada tersedia
+        if (count($this->model->checkEditData($email_lama, $email)) == 0) {
+            $nama = $req->nama;
+            $telepon = $req->telepon;
+            $alamat = $req->alamat;
+
+            // panggil model "editData"
+            $this->model->editData($email, $nama, $telepon, $alamat, $email_baru);
+
+            $error = 0;
+            $message = "Data Berhasil Diubah";
+        }
+        // jika data tidak tersedia
+        else {
+            $error = 1;
+            $message = "Data Gagal Diubah (Email Sudah Terpakai !)";
+        }
+
+        return response(["error" => $error, "message" => $message], http_response_code);
+    }
 }
