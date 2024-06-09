57dc1760465880300b61a3a4082418658bf013f8
veraafridita30 <veraafriditaa0130@gmail.com>
2024-06-09 12:17:15 +0700

    v

--- backend/app/Models/MPengunjung.php
+++ backend/app/Models/MPengunjung.php
@@ -11,5 +11,56 @@ class MPengunjung extends Model
     protected $table = "tb_pengunjung";
     
     // buat fungsi untuk menampilkan data pengunjung
-    
+    function viewData()
+    {
+        $query = $this->select("id AS id_pengunjung", "email AS email_pengunjung", "nama AS nama_pengunjung", "telepon AS telepon_pengunjung", "alamat AS alamat_pengunjung")->from($this->table)->orderBy("id");
+
+        return $query->get();
+    }
+
+    // buat fungsi untuk pencarian data pengunjung
+    function searchData($keyword)
+    {
+        $query = $this->select("id AS id_pengunjung", "email AS email_pengunjung", "nama AS nama_pengunjung", "telepon AS telepon_pengunjung", "alamat AS alamat_pengunjung")->from($this->table)
+        // pencarian email, telepon dan alamat harus sesuai dengan data
+        ->where('email', $keyboard)
+        ->orWhere('telepon', $keyword)
+        ->orWhere('alamat', $keyword)
+        // pencarian nama sesuai dengan karakter yang ada pada data
+        ->orWhereRaw('nama LIKE ?', ["%$keyword%"])
+        ->orderBy("id");
+
+        return $query->get();
+    }
+
+    // buat fungsi cek simpan data
+    function checkSaveData($email)
+    {
+        $query = $this->select("email")->from($this->table)->where("email", "=", $email);
+
+        return $query->get();
+    }
+
+    // buat fungsi untuk simpan data
+    function saveData($email, $nama, $telepon, $alamat)
+    {
+        $this->insert([
+            "email" => $email,
+            "nama" => $nama,
+            "telepon" => $telepon,
+            "alamat" => $alamat,
+        ]);
+    }
+
+    // buat fungsi untuk check data (berdasarkan email)
+    function checkData($email)
+    {
+        // $query = $this->select("id")->where("email", "=", $email);
+        $query = $this->select("id")
+        ->whereRaw("TO_BASE64(email) = '$npm'");
+
+        return $query->get();
+    }
+
+    // buat fungsi untuk hapus data
 }
