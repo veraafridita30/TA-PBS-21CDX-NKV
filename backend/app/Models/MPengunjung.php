03f95d72852ce2c3b95b541f9ecbecc47e129571
veraafridita30 <veraafriditaa0130@gmail.com>
2024-06-09 11:36:12 +0700

    update vera

--- backend/app/Models/MPengunjung.php
+++ backend/app/Models/MPengunjung.php
@@ -6,33 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MPengunjung extends Model
-
 {
-    // Buat variabel untuk inisialisasi tabel
+    // buat variabel untuk inisialisasi tabel
     protected $table = "tb_pengunjung";
-
-    // buat fungsi untuk menampilkan data mahasiswa
-    function viewData()
-    {
-        $query = $this->select("id AS id_pengunjung,", "nama AS nama_pengunjung", "telepon AS telepon_pengunjung", "alamat AS alamat_pengunjung")->from($this->table)->orderBy("id");
-
-        return $query->get();
-    }
-
-    // buat fungsi untuk pencarian data pengunjung
-    function searchData($keyword)
-    {
-        $query = $this->select("id AS id_pengunjung,", "nama AS nama_pengunjung", "telepon AS telepon_pengunjung", "alamat AS alamat_pengunjung")->from($this->table)
-        // pencarian nama, telepon dan alamat harus sesuai dengan data
-        ->where('nama', $keyword)
-        ->orWhere('telepon', $keyword)
-        ->orWhere('alamat', $keyword)
-        // pencarian nama sesuai dengan karakter yang ada pada data
-        ->orWhereRaw('nama LIKE?',["%$keyword%"])
-        ->orderBy("id");
-
-        return $query->get();
-    }
-
+    
+    // buat fungsi untuk menampilkan data pengunjung
     
 }
